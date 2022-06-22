240442f5c69c7412e6c143cc3178a528402b6039
Your Name <you@example.com>
2022-06-22 17:08:18 +0530

    [Megha] Add: Checking Employee Wage Is Done

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -11,4 +11,27 @@
         console.log("employee is Absent");
     }
 }
-console.log("Hello")
+
+//Uc2 switch case
+{
+const Is_Fulltime=2;
+const Is_Parttime=1;
+const Parttime_Hours=4;
+const Fulltime_Hours=8;
+const Wage_Per_Hours=20;
+{
+    let empHrs = 0;
+    let empCheck = Math.floor(Math.random()*10)%2;
+    switch(empCheck){
+        case Is_Parttime:
+            empHrs=Parttime_Hours;
+            break;
+        case Is_Fulltime:
+           empHrs=Fulltime_Hours;
+        default:
+            empHrs=0;
+    }
+    let empWage = empHrs*Wage_Per_Hours;
+    console.log("calculate empwage: "+empWage);
+}
+}
