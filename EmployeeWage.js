d551a84562f355c4142c75f55ed13f5074fd2ab2
Your Name <you@example.com>
2022-06-23 14:07:58 +0530

    [Megha] Add: Calculate Employee wage till condition is reached

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -16,6 +16,7 @@ const PART_TIME_HOURS=4;
 const FULL_TIME_HOUR=8;
 const WAGE_PER_HOUR=20;
 const NUM_OF_WORKING_DAYS=20;
+const MAX_HRS_IN_MONTH=160;
 {
     function getWorkingHours(empCheck)
     {
@@ -30,11 +31,13 @@ const NUM_OF_WORKING_DAYS=20;
         }
     }
     let totalEmpHrs=0;
-    for(let day=0; day<NUM_OF_WORKING_DAYS; day++)
+    let totalWorkingDays=0;
+   while(totalEmpHrs<= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
     {
+        totalWorkingDays++;
         let empCheck=Math.floor(Math.random() * 10) % 3;
         totalEmpHrs += getWorkingHours(empCheck);
     }
     let empWage=totalEmpHrs * WAGE_PER_HOUR;
-    console.log("Total Hours: " + totalEmpHrs +"\nEmployee Wage: " + empWage);
+    console.log("Total Days: " + totalWorkingDays + "\nTotal Hours: " + totalEmpHrs +"\nEmployee Wage: " + empWage);
 }
