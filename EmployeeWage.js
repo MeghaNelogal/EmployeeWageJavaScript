f39a91f0ec9f915292a8157ddc6b174730b91b1d
Your Name <you@example.com>
2022-06-23 17:07:01 +0530

    [Megha] Add: Employee Wage With Arrow

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -38,6 +38,8 @@ const MAX_HRS_IN_MONTH=160;
     let totalEmpHrs=0;
     let totalWorkingDays=0;
     let empDailyWageArr=new Array();
+    let empDailyWageMap=new Map();
+    let empDailyHrsMap=new Map();
    while(totalEmpHrs<= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
     {
         totalWorkingDays++;
@@ -45,6 +47,8 @@ const MAX_HRS_IN_MONTH=160;
         let empHrs=getWorkingHours(empCheck);
         totalEmpHrs += empHrs;;
         empDailyWageArr.push(calcDailyWage(empHrs));
+        empDailyHrsMap.set(totalWorkingDays,empHrs);
+        empDailyWageMap.set(totalWorkingDays,calcDailyWage(empHrs));
     }
     let empWage = calcDailyWage(totalEmpHrs);
     console.log("UC6 - Total Days: " + totalWorkingDays + "\nTotal Hours: " + totalEmpHrs +"\nEmployee Wage: " + empWage);
@@ -106,7 +110,26 @@ const MAX_HRS_IN_MONTH=160;
     }
     console.log("\nUC 7G - Number Of Days Employee Worked: "+empDailyWageArr.reduce(totalDaysWorked, 0));
 
-    //UC 8 - Map Functions
-    let empDailyWageMap=0;
-    console.log("\nUC8 - Employee Wage Map TotalHrs: "+Array.from(empDailyWageMap.Values()).reduce(totalWages,0));
+    //UC 8 - Map Function
+    console.log("\nUC 8 - Employee Wage Map TotalHrs: "+Array.from(empDailyWageMap.values()).reduce(totalWages,0));
+
+    //UC 9 - Arrow Functions
+    const findTotal = (totalVal, dailyVal) =>{
+        return totalVal + dailyVal;
+    }
+    let totalHours = Array.from(empDailyHrsMap.values()).filter(dailyHours =>dailyHours > 0).reduce(findTotal,0);
+    let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal,0);
+    console.log("UC 9A - Employee Wage With Arrow: "+"Total Hours: "+totalHours+"Total Wages: "+totalSalary);
+
+    let nonWorkingDays = new Array();
+    let partWorkingDays = new Array();
+    let fullWorkingDays = new Array();
+    empDailyHrsMap.forEach( (value, key, map)=> {
+        if(value == 8) fullWorkingDays.push(key);
+        else if (value == 4) partWorkingDays.push(key);
+        else nonWorkingDays.push(key);
+    });
+    console.log("Full Working Days: "+fullWorkingDays);
+    console.log("Part Working Days: "+partWorkingDays);
+    console.log("Non Working Days: "+nonWorkingDays);
 }
