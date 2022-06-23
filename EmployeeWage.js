9a7ac8b88df462489829dabc9424bec2b4403bc5
Your Name <you@example.com>
2022-06-23 13:48:50 +0530

    [Megha] Add: Calculate Employee Wage for month Asuming 20 days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,61 +1,40 @@
-//uc1 if else condition
 {
-    const Is_Present=1
-    let empCheck = Math.floor(Math.random()*10)%2;
-    
-    if(empCheck==Is_Present)
+    const IS_ABSENT=0
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if(empCheck==IS_ABSENT)
     {
-        console.log("employee is  Present");
-    
-    }else{
-        console.log("employee is Absent");
+        console.log("EMPLOYEE IS ABSENT.");
+    }
+    else
+    {
+        console.log("EMPLOYEE IS PRESENT.");
     }
 }
-
-//Uc2 switch case
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOUR=8;
+const WAGE_PER_HOUR=20;
+const NUM_OF_WORKING_DAYS=20;
 {
-const Is_Fulltime=2;
-const Is_Parttime=1;
-const Parttime_Hours=4;
-const Fulltime_Hours=8;
-const Wage_Per_Hours=20;
-{
-    let empHrs = 0;
-    let empCheck = Math.floor(Math.random()*10)%2;
-    switch(empCheck){
-        case Is_Parttime:
-            empHrs=Parttime_Hours;
-            break;
-        case Is_Fulltime:
-           empHrs=Fulltime_Hours;
-        default:
-            empHrs=0;
-    }
-    let empWage = empHrs*Wage_Per_Hours;
-    console.log("calculate empwage: "+empWage);
-}
-}
-
-//Uc3 using function calculate emp wage
-{
-    const Is_Parttime = 1;
-    const Is_Fulltime = 2;
-    const Wage_Per_Hours=20;
-    function getWorkingHours(empCheck){
+    function getWorkingHours(empCheck)
+    {
         switch(empCheck)
         {
-            case Is_Parttime:
-                return Parttime_Hours;
-                
-            case Is_Fulltime:
-                return Fulltime_Hours;
+            case IS_PART_TIME:
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOUR;
             default:
                 return 0;
         }
     }
-       
-        let empCheck = Math.floor(Math.random()*10)%2;
-        let empHrs = getWorkingHours(empCheck);
-        let empWage = empHrs*Wage_Per_Hours;
-        console.log("Total Hrs: "+empHrs+"calculate empwage: "+empWage);
+    let totalEmpHrs=0;
+    for(let day=0; day<NUM_OF_WORKING_DAYS; day++)
+    {
+        let empCheck=Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
     }
+    let empWage=totalEmpHrs * WAGE_PER_HOUR;
+    console.log("Total Hours: " + totalEmpHrs +"\nEmployee Wage: " + empWage);
+}
